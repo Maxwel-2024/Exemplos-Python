1c33318e75b445b641375461bfc38bb8128db18d
Maxwel-2024 <maxwelsena@gmail.com>
2025-04-14 08:12:12 -0300

    Exercício 6

---
+++ Exercício 6.py
@@ -0,0 +1,11 @@
+choveu_hoje = False
+print(choveu_hoje)
+print(type(choveu_hoje))  # Verifica se é um valor booleano (bool) ou seja se é True(verdadeiro) ou False(falso)
+
+numero = 10
+print(numero)
+print(type(numero))  # Verifica se é um número inteiro (int)
+
+numero = "dez"
+print(numero)
+print(type(numero))  # Verifica se agora é uma string (str)
