4c8dad2f2e0a7822ece5aa57964bad11ef51ddeb
Maxwel-2024 <maxwelsena@gmail.com>
2025-04-14 16:38:59 -0300

    Correção do texto do código de teste

--- Testes.py
+++ Testes.py
@@ -1,6 +1,6 @@
 nome = "João"
-print("Tudo bem," ,nome, "Como vai seu dia?") #Contatenando ou seja, juntar os textos com vírgulas. 
+print("Tudo bem," ,nome, "? Como vai seu dia ?") #Contatenando ou seja, juntar os textos com vírgulas. 
 # O print com vírgula adiciona um espaço entre os elementos.
 
-print("Tudo bem, " + nome + "? Como vai seu dia?") #Contatenando com +
+print("Tudo bem, " + nome + " ? Como vai seu dia ?") #Contatenando com +
 # O + é usado para concatenar (juntar) strings (textos), enquanto a vírgula já adiciona um espaço entre os elementos.
